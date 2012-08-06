f052fb590d1dc83f6277f2f8dde87a8043c3a27b
GordonDiggs <gordon@gordondiggs.com>
2012-08-06 00:10:02 -0400

    hoping this works

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,6 @@ module Ellis
     config.assets.initialize_on_precompile = false
 
     # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
-    config.assets.precompile += ['active_admin.css.scss', 'active_admin.js'] 
+    config.assets.precompile += ['active_admin.css.scss', 'active_admin.css', 'active_admin.js']
   end
 end

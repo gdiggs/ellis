ef0cd3e9e487c8a43e35b400d2d6a29912492256
Gordon Diggs <gordon@gordondiggs.com>
2015-06-15 14:49:14 -0400

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :collection
 
-  MAX_FIELDS = 10
+  MAX_FIELDS = 100
   
   validates :num_fields, :numericality => { :greater_than => 0, :less_than_or_equal_to => MAX_FIELDS }
   validates_presence_of :num_fields, :collection_id

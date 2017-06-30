1787d1dccabd580d7eb3219ec2a7ece7144c9385
Gordon Diggs <gordon@gordondiggs.com>
2017-06-30 11:53:20 -0400

    fixme

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :collection
 
+  # FIXME: Move configuration to environment
   MAX_FIELDS = 100
-  
+
   validates :num_fields, :numericality => { :greater_than => 0, :less_than_or_equal_to => MAX_FIELDS }
   validates_presence_of :num_fields, :collection_id
 end
